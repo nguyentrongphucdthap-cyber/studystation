bde3f7f64da85e3e0db5dc34ee8b890d94d95756
1PhoPhuc <nguyentrongphucdthap@gmail.com>
2025-12-12 15:17:30 +0700

    Update gatekeeper.js

--- gatekeeper.js
+++ gatekeeper.js
@@ -420,6 +420,20 @@ export async function deleteExam(examId) {
 export function getSubjects() {
     return [
         {
+            id: 'math',
+            name: 'Toán',
+            color: 'text-red-600',
+            bg: 'bg-red-50 dark:bg-red-900/30',
+            icon: '<svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-red-600 dark:text-red-400" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 7h6m0 10v-3m-3 3h.01M9 17h.01M9 14h.01M12 14h.01M15 11h.01M12 11h.01M9 11h.01M7 21h10a2 2 0 002-2V5a2 2 0 00-2-2H7a2 2 0 00-2 2v14a2 2 0 002 2z" /></svg>'
+        },
+        {
+            id: 'chem',
+            name: 'Hóa học',
+            color: 'text-cyan-600',
+            bg: 'bg-cyan-50 dark:bg-cyan-900/30',
+            icon: '<svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-cyan-600 dark:text-cyan-400" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19.428 15.428a2 2 0 00-1.022-.547l-2.387-.477a6 6 0 00-3.86.517l-.318.158a6 6 0 01-3.86.517L6.05 15.21a2 2 0 00-1.806.547M8 4h8l-1 1v5.172a2 2 0 00.586 1.414l5 5c1.26 1.26.367 3.414-1.415 3.414H4.828c-1.782 0-2.674-2.154-1.414-3.414l5-5A2 2 0 009 10.172V5L8 4z" /></svg>'
+        },
+        {
             id: 'bio',
             name: 'Sinh học',
             color: 'text-emerald-600',
